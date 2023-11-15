d1853c823c015be7a270da86e9bba8a7570bccad
annahritskova <annagovorova97@gmail.com>
2023-11-15 09:25:24 +0100

    Homework #19

--- config/testConfig.js
+++ config/testConfig.js
@@ -7,5 +7,10 @@ export const testConfig = {
   httpCredentials: {
     username: process.env.HTTP_CREDENTIAL_USER_NAME,
     password: process.env.HTTP_CREDENTIAL_PASSWORD
+  },
+  reporters: {
+    testomat: {
+      key: process.env.TESTOMAT_KEY
+    }
   }
 };
---
+++ src/data/storageState.js
@@ -0,0 +1,1 @@
+export const STORAGE_STATE_USER_PATH = ".//state/garage-state.json";
