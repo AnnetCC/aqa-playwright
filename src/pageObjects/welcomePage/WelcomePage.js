2f66bb206c60c4fd2cb9ff28ab57cb2662dfe8c6
annahritskova <annagovorova97@gmail.com>
2023-11-15 09:25:24 +0100

    Homework #19

--- src/pageObjects/welcomePage/WelcomePage.js
+++ src/pageObjects/welcomePage/WelcomePage.js
@@ -1,16 +1,24 @@
-import BasePage from '../BasePage.js'
-import SignUpForm from '../components/SignUpForm.js'
+import BasePage from "../BasePage.js";
+import SignUpForm from "../components/SignUpForm.js";
+import SignInForm from "../components/SignInForm.js";
 
 export default class WelcomePage extends BasePage {
-  _signUpButton = '.hero-descriptor_btn'
+  _signUpButton = ".hero-descriptor_btn";
+  _signInButton = ".header_signin";
 
-  constructor (page) {
-    super(page, '/', page.locator('button', { hasText: 'Guest log in' }))
-    this.signUpButton = this._page.locator(this._signUpButton, { hasText: 'Sign up' })
+  constructor(page) {
+    super(page, "/", page.locator("button", {hasText: "Guest log in"}));
+    this.signUpButton = this._page.locator(this._signUpButton, {hasText: "Sign up"});
+    this.signInButton = this._page.locator(this._signInButton, {hasText: "Sign in"});
   }
 
-  async openSignUpPopup () {
-    await this.signUpButton.click()
-    return new SignUpForm(this._page)
+  async openSignUpPopup() {
+    await this.signUpButton.click();
+    return new SignUpForm(this._page);
+  }
+
+  async openSignInPopup() {
+    await this.signInButton.click();
+    return new SignInForm(this._page);
   }
 }
---
+++ tests/garage.spec.js
@@ -0,0 +1,13 @@
+import {expect} from "@playwright/test";
+import {test} from "../src/fixtures/garage.fixtures.js";
+
+test.describe("Test garage page @Sadb7a718", () => {
+  test("Verify that label Garage exists @T732cfb5c", async ({userGaragePage}) => {
+    await expect(userGaragePage.garageLabel).toBeVisible();
+  });
+
+  test("Verify that Fuel expenses tab can be opened from garage page @Tf35eae83", async ({userGaragePage}) => {
+    await userGaragePage.fuelExpensesButton.click();
+    await expect(userGaragePage.expensesLabel, "Fuel expenses tab is opened").toBeVisible();
+  });
+});
