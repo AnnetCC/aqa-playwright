679f5f25c0363c093ed526adef281024ec7b90ed
annahritskova <annagovorova97@gmail.com>
2023-12-11 08:13:06 +0200

    changed env variables for docker-compose.yml

--- tests/setup/loginAsUser.setup.js
+++ tests/setup/loginAsUser.setup.js
@@ -1,9 +1,9 @@
-import { test } from "@playwright/test"
-import { USERS } from "../../src/data/users.js"
+import {test} from "@playwright/test"
+import {USERS} from "../../src/data/users.js"
 import WelcomePage from "../../src/pageObjects/welcomePage/WelcomePage.js"
-import { STORAGE_STATE_USER_PATH } from "../../src/data/storageState.js"
+import {STORAGE_STATE_USER_PATH} from "../../src/data/storageState.js"
 
-test("Login as user and save storage state @Tddfe01d6", async ({ page, context }) => {
+test("Login as user and save storage state @Tddfe01d6", async ({page, context}) => {
   const welcomePage = new WelcomePage(page)
   await welcomePage.navigate()
   const popup = await welcomePage.openSignInPopup()
